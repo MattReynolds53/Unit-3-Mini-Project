1df58a65b9242bded2b41265c0c6e85b7554af61
Matt Reynolds <mattreynolds53@gmail.com>
2021-11-10 11:00:56 -0600

    added if statements and wins, losses, and ties variables

--- Assets/script.js
+++ Assets/script.js
@@ -1,37 +1,63 @@
-let choice = prompt("Welcome to your game of Rock, Paper, Scissors. Please enter your choice of either R, P, or S");
+playRPS ()
+
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
+function playRPS () {
+    let choice = prompt("Welcome to your game of Rock, Paper, Scissors. Please enter your choice of either R, P, or S");
 
 console.log(choice);
 
 let computerChoice = ["R", "P", "S"];
-let finalComputerAnswer = Math.floor(Math.random()*computerChoice.length);
-console.log(finalComputerAnswer)
+let randomChoice = Math.floor(Math.random()*computerChoice.length);
+console.log(randomChoice)
+let finalComputerAnswer = computerChoice[randomChoice]
 
 if (choice === finalComputerAnswer) {
     console.log("It's a tie!");
+    ties++;
+    alert("It's a tie!");
 } else if (choice === "P" && finalComputerAnswer === "R") {
     console.log("You win!");
+    wins++;
+    alert("You win! You're smarter than your computer!");
 } else if (choice === "P" && finalComputerAnswer === "S") {
     console.log("You lost :(");
+    losses++;
+    alert("You lose lol");
 } else if (choice === "S" && finalComputerAnswer === "P") {
     console.log("You win!");
+    wins++;
+    alert("You win! You're smarter than your computer!");
 } else if (choice === "S" && finalComputerAnswer === "R") {
     console.log("You lost :(");
+    losses++;
+    alert("You lose lol");
 } else if (choice === "R" && finalComputerAnswer === "S") {
     console.log("You win!");
+    wins++;
+    alert("You win! You're smarter than your computer!");
 } else {
     console.log("You lost :(");
+    losses++;
+    alert("You lose lol");
 }
 
+alert(
+    `Stats:
+Wins: ${wins}
+Losses: ${losses}
+Ties: ${ties}`
+);
+alert("Wins: " + wins + "Losses: " + losses + "Ties: " + ties)
 
+let replayChoice = confirm("Would you like to play again?");
 
-// let result = prompt("Computer choice: " + Math.floor(Math.random()*computerChoice.length);
-
-// console.log(result);
-
-// // alert("values:" + choice + " " + computerAnswer);
-
-// let replay = prompt("Would you like to play again?")
-
-// console.log(replay)
-
+if (replayChoice === true) {
+    playRPS ()
+} else {
+    return;
+}
+}
 
